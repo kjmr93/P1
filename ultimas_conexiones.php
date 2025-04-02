2a21cf718bc4b1133c7ebccf6ace81e046c084a3
Kike <72933928+kjmr93@users.noreply.github.com>
2025-04-02 17:16:00 +0000

    ultimas conexiones3

--- ultimas_conexiones.php
+++ ultimas_conexiones.php
@@ -51,7 +51,7 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Usuarios Inactivos</title>
+    <title>Últimas Conexiones</title>
     <link rel="stylesheet" href="styles.css">
     <style>
         body {
