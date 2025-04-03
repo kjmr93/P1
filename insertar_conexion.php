edf249c0675bf66b4a0c36773fd87b9f60a862ee
Kike <72933928+kjmr93@users.noreply.github.com>
2025-04-03 10:56:11 +0000

    insertar conexiones

--- insertar_conexion.php
+++ insertar_conexion.php
@@ -25,6 +25,7 @@ $snap_opera = $_POST['snap_opera'];
 $windows = $_POST['windows'];
 $serial = $_POST['serial'];
 $model = $_POST['model'];
+$macssid = $_POST['macssid']; // Nueva línea para capturar el valor de macssid
 
 // Extraer el texto entre los dos puntos y la primera coma de "connexions"
 $nomusuari = '';
@@ -34,18 +35,33 @@ if (strpos($connexions, ':') !== false && strpos($connexions, ',') !== false) {
     $nomusuari = substr($connexions, $start, $end - $start); // Extraer el texto
 }
 
-// Insertar datos en la base de datos
-$sql = "INSERT INTO datos (mac, version, admins, maquina, nomusuari, connexions, data_restauracio, restriccio, snap_installat, snap_vpns, snap_opera, windows, serial, model)
-VALUES ('$mac', '$version', '$admins', '$maquina', '$nomusuari', '$connexions', '$data_restauracio', '$restriccio', '$snap_installat', '$snap_vpns', '$snap_opera', '$windows', '$serial', '$model')";
+// Obtener el valor de "restriccio" de la tabla "usuaris" basado en "nomusuari"
+$restriccio_usuari = 0; // Valor predeterminado en caso de que no se encuentre el usuario
+$sql_restriccio = "SELECT restriccio FROM usuaris WHERE nomusuari = ?";
+$stmt = $conn->prepare($sql_restriccio);
+$stmt->bind_param("s", $nomusuari);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $restriccio_usuari = $row['restriccio']; // Asignar el valor de "restriccio" si se encuentra
+}
+
+$stmt->close();
+
+// Insertar datos en la tabla "historial"
+$sql = "INSERT INTO historial (mac, version, admins, maquina, nomusuari, connexions, data_restauracio, restriccio, snap_installat, snap_vpns, snap_opera, windows, serial, model, macssid, restriccio_usuari)
+VALUES ('$mac', '$version', '$admins', '$maquina', '$nomusuari', '$connexions', '$data_restauracio', '$restriccio', '$snap_installat', '$snap_vpns', '$snap_opera', '$windows', '$serial', '$model', '$macssid', '$restriccio_usuari')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Datos insertados correctamente";
+    echo "Datos insertados correctamente en la tabla historial";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
 
-header("Location: datos.php");
+header("Location: historial.php");
 exit();
 ?>
